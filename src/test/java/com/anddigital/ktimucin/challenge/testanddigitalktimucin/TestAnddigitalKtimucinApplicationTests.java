5167b8d9ff11f5f7aa1969b48d969a8e62ffa045
ktimucin <kubilay.timucin@gmail.com>
2018-11-08 09:56:03 +0000

    first test is added. HTTP 200 expected but since no endpoint was set failed. Now time to make it green(Create controller, dao etc.)

--- src/test/java/com/anddigital/ktimucin/challenge/testanddigitalktimucin/TestAnddigitalKtimucinApplicationTests.java
+++ src/test/java/com/anddigital/ktimucin/challenge/testanddigitalktimucin/TestAnddigitalKtimucinApplicationTests.java
@@ -5,12 +5,26 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.restassured.RestAssured;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestAnddigitalKtimucinApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	//Just check whether /phones endpoint is up.
+	@Test
+	public void testGetPhonesEndPoint() {
+		
+		RestAssured.baseURI="http://localhost:8080";
+		
+		RestAssured.given().when().get("phones").
+					then().assertThat().statusCode(200);
+		
+		
 	}
 
 }
