8c945757313b464a77ca7fb6580f840734739e8b
ktimucin <kubilay.timucin@gmail.com>
2018-11-08 10:06:44 +0000

    End point is up but second Test failed to retrieve bnot empty list

--- src/test/java/com/anddigital/ktimucin/challenge/testanddigitalktimucin/TestAnddigitalKtimucinApplicationTests.java
+++ src/test/java/com/anddigital/ktimucin/challenge/testanddigitalktimucin/TestAnddigitalKtimucinApplicationTests.java
@@ -1,11 +1,17 @@
 package com.anddigital.ktimucin.challenge.testanddigitalktimucin;
 
+import static org.junit.Assert.assertTrue;
+
+import javax.validation.constraints.AssertTrue;
+
+import org.hamcrest.core.IsNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,5 +32,19 @@ public class TestAnddigitalKtimucinApplicationTests {
 		
 		
 	}
+	//Just check whether get phones is returning empty or not.
+	//Aim is create DAO and return proper phones
+		@Test
+		public void isGetPhonesAList() {
+			
+			RestAssured.baseURI="http://localhost:8080";
+			
+			String phones = RestAssured.given().when().get("phones").
+						then().assertThat().statusCode(200)
+						.and().extract().response().asString();
+			assertTrue(!phones.equalsIgnoreCase(""));
+			
+			
+		}
 
 }
